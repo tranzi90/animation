ca117706310c909604cc56c6576d0ac836d50b00
denis_levchenko <tranzi90@gmail.com>
2024-04-24 15:26:01 +0300

    start animation by tap

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -32,7 +32,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     ).animate(
       CurvedAnimation(
         parent: boxController,
-        curve: Curves.linear,
+        curve: Curves.easeInOut,
       ),
     );
 
@@ -61,8 +61,10 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
 
   onTap() {
     if (catController.status == AnimationStatus.completed) {
+      boxController.forward();
       catController.reverse();
     } else if (catController.status == AnimationStatus.dismissed) {
+      boxController.stop();
       catController.forward();
     }
   }
@@ -142,7 +144,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
         builder: (BuildContext context, Widget? child) {
           return Transform.rotate(
             alignment: Alignment.topRight,
-            angle: boxAnimation.value,
+            angle: -boxAnimation.value,
             child: child,
           );
         },
